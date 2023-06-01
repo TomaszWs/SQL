34fe214b6800b55b62b66d4d7ed0b8668c7ab764
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-06-01 15:21:12 +0200

    correct spelling

--- Prison/Prison_funkcje.sql
+++ Prison/Prison_funkcje.sql
@@ -1,21 +1,21 @@
 --Funkcja oblicza i zwraca bieżący wiek więźnia/ów na podstawie daty urodzenia (w przeciwieństwie do wieku przyjęcia do więzienia)
-CREATE FUNCTION ObliczWiek (@dataUrodzenia DATE) RETURNS INT 
+CREATE FUNCTION ObliczWiek (@data_urodzenia DATE) RETURNS INT 
 AS
 BEGIN
 DECLARE @wiek INT;
-SET @wiek = DATEDIFF(YEAR, @dataUrodzenia, GETDATE());
+SET @wiek = DATEDIFF(YEAR, @data_urodzenia, GETDATE());
 RETURN @wiek;
 END
 
-SELECT Imie, Nazwisko, dbo.ObliczWiek(data_urodzenia) AS Wiek FROM Wiezien;
+SELECT imie, nazwisko, dbo.ObliczWiek(data_urodzenia) AS Wiek FROM Wiezien;
 
 --Funkcja zwraca najdłuższy wyrok w postaci tabeli wybierając najwyższy wynik z posortowanej malejąco tabeli wyrok 
-CREATE FUNCTION PobierNajdluzszyWzyrok() RETURNS TABLE 
+CREATE FUNCTION pobierz_najdluzszy_wyrok() RETURNS TABLE 
 AS
 RETURN
 (SELECT TOP 1 * FROM Wyrok ORDER BY czas_trwania DESC);
 
-SELECT * FROM dbo.PobierzNajdłużejTrwającyWyrok();
+SELECT * FROM dbo.pobierz_najdluzszy_wyrok();
 
 INSERT INTO Wyrok(data_wydania,czas_trwania,popelnione_przestepstwo,id_wiezien) VALUES('2023-01-01', 1000000, 'Kradziez',6)
-SELECT * FROM dbo.PobierzNajdłużejTrwającyWyrok();
+SELECT * FROM dbo.pobierz_najdluzszy_wyrok();
