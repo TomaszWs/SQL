6eb3b5f0cdf5729b5c671ff68b140ddb15c9890e
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-06-01 16:09:45 +0200

    add join

--- Prison/Prison_widok.sql
+++ Prison/Prison_widok.sql
@@ -1,8 +1,9 @@
 
---Widok zbiera informacje o wizycie i wiezniu
-CREATE VIEW WizytacjeWiezniow 
+--Widok zbiera informacje o wizycie, wiezniu oraz osobie odwiedzającej
+CREATE VIEW WizytacjeWiezniow
 AS 
-SELECT Wizytacja.id_wizyta, Wizytacja.data_godzina, Wizytacja.powod_wizyta, Wiezien.imie, Wiezien.nazwisko, Wiezien.data_urodzenia, Wiezien.narodowosc
-FROM Wizytacja JOIN Wiezien ON Wizytacja.id_wiezien = Wiezien.id_wiezien;
+SELECT Wizytacja.data_godzina, Wizytacja.powod_wizyta, Wiezien.imie, Wiezien.nazwisko, Wiezien.data_urodzenia, Wiezien.narodowosc, 
+Odwiedzajacy.imię, Odwiedzajacy.nazwisko, Odwiedzajacy.relacja_z_wiezniem
+FROM Wizytacja JOIN Wiezien ON Wizytacja.id_wiezien = Wiezien.id_wiezien JOIN Odwiedzajacy ON Wizytacja.id_wizyta = Odwiedzajacy.id_wizyta
 
 SELECT * FROM WizytacjeWiezniow;
