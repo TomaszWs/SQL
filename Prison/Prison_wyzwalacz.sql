799852dd0743327f7d3abcc6de87bb75846d4bf7
Tomasz Wąs <tomasz.p.was@gmail.com>
2023-06-03 18:07:34 +0200

    Update trigger

--- Prison/Prison_wyzwalacz.sql
+++ Prison/Prison_wyzwalacz.sql
@@ -1,3 +1,9 @@
+/*
+ * @author Tomasz Wąs
+ * @create date 2023-06-02 12:18:59
+ * @modify date 2023-06-03 18:06:40
+ * @desc Procedures for prison database
+ */
 
 --do wyzwalacza potrzebna jest nowa tabela, która będzie modyfikowana przez wyzwalacz
 CREATE TABLE dane_wiezienia (liczba_wyrokow INT, liczba_wiezniow INT, liczba_odwiedzajacych INT);
@@ -17,6 +23,6 @@ UPDATE dane_wiezienia
 SET liczba_wyrokow = (SELECT COUNT(*) FROM Wyrok);
 END;
 
-
+-- Przykładowe użycie wyzwalacza:
 DELETE FROM Wyrok WHERE id_Wyrok = 6;
 SELECT * FROM dane_wiezienia
